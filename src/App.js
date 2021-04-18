3f5c7b984aebee89a2cec03a3654c3a13fbe66ef
mohammad hamayel <mohammadjhamayel3@gmail.com>
2021-04-18 22:46:24 +0300

    make personArray of JSX object

--- src/App.js
+++ src/App.js
@@ -58,15 +58,6 @@ class App extends Component {
               />
             })
           }
-          <Person name={this.state.persons[0].name } age= {this.state.persons[0].age}/>
-          <Person name={this.state.persons[1].name} age= {this.state.persons[1].age}/>
-          <Person 
-          name={this.state.persons[2].name} 
-          age= {this.state.persons[2].age} 
-          click={this.switchNameHandler.bind(this,'Ola')}
-          changed={this.nameChangedHandler}
-          > my habit is racing </Person>
-          <Person name={this.state.otherState} age= {this.state.oAge} >just state </Person>
         </div> 
       );
     }
