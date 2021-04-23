94d0fc3790b108de1728eafa491903d20c56230c
mohammad hamayel <mohammadjhamayel3@gmail.com>
2021-04-23 13:42:21 +0300

    add dynamic css classes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component} from 'react';
 import './App.css';
-import person from './Person/Person';
-import Person from './Person/Person'
+import Person from './Person/Person';
 
 class App extends Component {
   state =  {
@@ -43,7 +42,8 @@ class App extends Component {
   }
   render() {
     const style= {
-      backgroundColor:'white',
+      backgroundColor:'green',
+      color:'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -65,12 +65,19 @@ class App extends Component {
             })}
         </div> 
       );
+      style.backgroundColor= 'red';
     }
-
+    const classes =[];
+    if(this.state.persons.length <=2){
+      classes.push('red');
+    }
+    if(this.state.persons.length <=1){
+      classes.push('bold');
+    }
     return (
       <div className="App">
         <h1>Hello There</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button 
         style={style}
         onClick={() => this.togglePersonsHandler()}>Switch Name</button>
