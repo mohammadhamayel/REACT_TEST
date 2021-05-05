b4e4a5ff3f733b03e165fdf26e26cdb23f2ce801
mohammadjhamayel3@gmail.com <mohammadjhamayel3@gmail.com>
2021-05-05 15:31:07 +0300

    update life cycle and useEffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import classes from './Cockpit.css'
 
 const cockpit = (props) => {
+    useEffect(() => {
+      console.log('[Cockpit.js] useEffect');
+      setTimeout(() => {
+        
+      }, 1000);
+    })
     const assignClasses =[];
     let btnClass = '';
-    if(props.showPerson){
+    if(props.showPersons){
         btnClass = classes.Red;
     }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -24,17 +24,23 @@ class App extends Component {
     return state;
   }
 
-  // componentWillMount(){
-  //   console.log('[Spp.jd] componentDidMount');
-  // }
-
   componentDidMount(){
     console.log('[App.js] componentDidMount')
   }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate(){
+    console.log('[App.js] componentDidUpdate')
+  }
+
   nameChangedHandler = (event, id) =>{
     const personIndex = this.state.persons.findIndex(p => {
-      return p.id ===id;
-    });
+    return p.id ===id;
+  });
 
    const person = {...this.state.persons[personIndex]};
    //const person = Object.assign({},this.state.persons[personIndex]);
