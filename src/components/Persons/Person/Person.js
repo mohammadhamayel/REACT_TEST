76eb3a11d3a69e8587d0ffb62ecd563cb957f754
mohammad hamayel <mohammadjhamayel3@gmail.com>
2021-05-07 14:30:37 +0300

    React.Fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,7 +5,7 @@ class Person extends Component {
     render(){
         console.log('[Person.js] rendering ...')
         return (
-        <Aux>
+        <React.Fragment>
                 <p onClick= {this.props.click} >
                     I'm {this.props.name} and I am {this.props.age } years old!
                 </p>
@@ -13,7 +13,7 @@ class Person extends Component {
                 <input type="text" 
                 onChange= {this.props.changed} 
                 value={this.props.name}/>
-        </Aux>)
+        </React.Fragment>)
       ;
     }
 }
