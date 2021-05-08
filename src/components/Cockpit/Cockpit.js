053d455f1b47416f86be61aea7d4d049d45c613d
mohammad hamayel <mohammadjhamayel3@gmail.com>
2021-05-08 11:48:26 +0300

    implement statuc context for class component and useContext for funcational component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,9 +1,11 @@
-import React, {createContext, useEffect, useRef} from 'react';
+import React, {useEffect, useRef, useContext} from 'react';
 import classes from './Cockpit.css'
 import AuthContext from '../../context/auth-context';
 const cockpit = (props) => {
   const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
 
+  console.log('[Cockpit.js] authenticated :  '+authContext.authenticated);
     useEffect(() => {
       // console.log('[Cockpit.js] useEffect');
       // setTimeout(() => {
@@ -43,9 +45,7 @@ const cockpit = (props) => {
             className={btnClass}
             onClick={props.clicked}> Switch Name
             </button>
-            <AuthContext.Consumer>
-              {context => <button onClick={context.login}>Log in</button>}
-            </AuthContext.Consumer>
+              <button onClick={authContext.login}>Log in</button>
         </div>
     );
 };
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -10,30 +10,29 @@ class Person extends Component {
         super(props);
         this.inputElementRef = React.createRef();
     }
+
+    static contextType = AuthContext;/// this context use in class-base component
+
     componentDidMount(){
         //this.inputElement.focus(); used for method 1
         this.inputElementRef.current.focus();
+        console.log('[Person.js] authenticated : '+this.context.authenticated);
     }
     render(){
         console.log('[Person.js] rendering ...')
         return (
         <Aux >
-            <AuthContext.Consumer>
-                {(context) => 
-                    context.authenticated ? <p>Authenticated!</p>: <p>Please Log in</p>
-                }
-            </AuthContext.Consumer>
-                {/* {this.props.isAuth? <p>Authenticated</p>: <p>Please log in</p>} used before  auth-context to implement authentication method */}
-                <p onClick= {this.props.click} >
-                    I'm {this.props.name} and I am {this.props.age } years old!
-                </p>
-                <p>{this.props.children}</p>
-                <input type="text" 
-                key="i3"
-                //ref={(inputEl) => {this.inputElement = inputEl}} passing ref throw this function method 1
-                ref={this.inputElementRef}
-                onChange= {this.props.changed} 
-                value={this.props.name}/>
+            { this.context.authenticated ? <p>Authenticated!</p>: <p>Please Log in</p>}                {/* {this.props.isAuth? <p>Authenticated</p>: <p>Please log in</p>} used before  auth-context to implement authentication method */}
+            <p onClick= {this.props.click} >
+                I'm {this.props.name} and I am {this.props.age } years old!
+            </p>
+            <p>{this.props.children}</p>
+            <input type="text" 
+            key="i3"
+            //ref={(inputEl) => {this.inputElement = inputEl}} passing ref throw this function method 1
+            ref={this.inputElementRef}
+            onChange= {this.props.changed} 
+            value={this.props.name}/>
         </Aux>)
       ;
     }
